b2b1941e2c279ec01e5f5eb393e8643eecef57ac
ibnaleem <ibnaleem@outlook.com>
2023-08-08 12:23:12 +0000

    English translations

--- collector.py
+++ collector.py
@@ -1,23 +1,24 @@
 # Copyright 2012-2023 The Collector (lite) - Mr.Chess
-import requests
+
+import requests, time, re
 from bs4 import BeautifulSoup
-import re
-import time
 from progress.bar import Bar
+
 URL = 'https://targetonionwebsites.onion'
 keywords = ['Keyword1', 'Keyword2', 'Keyword3']  # add your keywords
 visited_links = set()
 bad_sites = []
+
 def find_links(url):
     global visited_links
     global bad_sites
     try:
         response = requests.get(url)
     except Exception as e:
-        print(f"Error beim Zugriff auf URL {url}: {e}")
+        print(f"Error accessing URL {url}\n\n{e}") # changed to English + new lines for readabilty 
         return
     if response.status_code != 200:
-        print(f"Error beim Zugriff auf URL {url}: Statuscode {response.status_code}")
+        print(f"Error accessing URL {url}\n\nStatus code: {response.status_code}") # changed to English + new lines for readabilty 
         return
     visited_links.add(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -25,16 +26,16 @@ def find_links(url):
     for keyword in keywords:
         if keyword and keyword.lower() in response.text.lower():
             bad_sites.append(url)
-            print(f"BAD Site gefunden: {url}")
+            print(f"BAD Site Found: {url}") # changed to English
             break
-    with Bar('Verfolge Links...', max=len(links)) as bar:
+    with Bar('Follow Links...', max=len(links)) as bar: # changed to English
         for link in links:
             href = link['href']
             if not (href.startswith('http') or href.startswith('https')):
                 continue
             if href not in visited_links:
                 bar.next()
-                print(f"Besuche: {href}")
+                print(f"Visits: {href}") # changed to English
                 find_links(href)
                 time.sleep(2)
 find_links(URL)
